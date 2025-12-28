36fcf0b703fabb689ccf7be2f7a60fa81b77c4f7
mzabicka <mzabicka0110@gmail.com>
2025-12-28 14:25:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from langchain_openai import ChatOpenAI
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains import create_retrieval_chain, create_history_aware_retriever
 from langchain.chains.combine_documents import create_stuff_documents_chain
